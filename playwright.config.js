1d7a5e3309b86b3ccdda08cffa1dcf9386600647
olemoreflipflop <fil.olga.ev@gmail.com>
2023-03-01 13:19:24 +0300

    .\

--- playwright.config.js
+++ playwright.config.js
@@ -37,9 +37,8 @@ module.exports = defineConfig({
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
